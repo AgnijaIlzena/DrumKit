1e1ddf3aea367b3987a01c1fdf35e899bccffb4d
Agnija <ilzena.agnija@gmail.com>
2021-10-09 21:39:14 +0200

    all buttons with eventListeners by loop

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-document.querySelector("button").addEventListener("click", handleClick);
+//document.querySelector("button").addEventListener("click", handleClick);
+
+const buttons = document.querySelectorAll("button")
+buttons.forEach(function(currentBtn){
+    currentBtn.addEventListener("click", handleClick);
+})
+
+
+
+// for (let i = 0; i<document.querySelectorAll(".drum").length; i++) {
+
+// document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+// }
 
 function handleClick() {
     alert("I got clicked!");
